a58d507470f944a7fd3d255a0e5d8589aa770d0a
Kim Lavoie <kimlavoie@MacBook-Air-de-Kim.local>
2014-11-22 00:21:34 -0500

    Commented source

--- Source/as.py
+++ Source/as.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+
+# Import common functions and standard library modules
 from fonctions import Encryption, Decryption, DicoFromFile
 import sys, time, base64
 
+# Get and parse arguments
 entree = sys.argv[1]
 
 tab_entree = entree.split("#")
@@ -12,21 +15,26 @@ IDtgs = tab_entree[2]
 TS1 = str(int(time.time()))
 Duree1 = str(3600)
 
+# Parse cles.txt and create a dictionnary (Array map)
 keys = DicoFromFile()
 
+# Catch missing IDc
 try:
     Kc = keys[IDc]
 except KeyError:
     print "Identifiant inconnu"
     sys.exit(1)
     
+# Catch missing IDtgs
 try:
     Ktgs = keys[IDtgs]
 except KeyError:
     print "TGS inconnu"
     sys.exit(1)
 
+# Generate Ticket
 TICKETtgs = Encryption(Ktgs, IDc + "#" + ADc + "#" + IDtgs + "#" + TS1 + "#" + Duree1)
 
+# Send results in base64
 result = base64.b64encode(Encryption(Kc, TICKETtgs))
 print result
--- Source/decrypteur.py
+++ Source/decrypteur.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+
+# Import common functins and standard library modules
 from fonctions import Encryption, Decryption, DicoFromFile
 import sys, base64
 
+# Get and parse arguments, and cles.txt
 IDc = sys.argv[1]
 keys = DicoFromFile()
 cle = keys[IDc]
+
+# base64 to normal
 encrypte = base64.b64decode(sys.argv[2])
 
+# print decrypted Ticket
 print base64.b64encode(Decryption(cle, encrypte))
